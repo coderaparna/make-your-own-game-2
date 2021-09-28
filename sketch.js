3a9ca52190021f717d80c84b0df455975a83c397
coderaparna <78423184+coderaparna@users.noreply.github.com>
2021-09-28 08:25:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var bg;
+var spaceship, Spaceship;
+var meteor, Meteor;
+
+
+function preload(){
+ bg = loadImage("background.png");
+ spaceship = loadImage("spaceship.png");
+ meteor = loadImage("meteor.png");
+}
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  ground = createSprite(windowWidth/2,windowHeight/2,width*2,height*2);
+  ground.addImage(bg);
+  Spaceship = createSprite(width/2, height-250, 50, 60);
+  Spaceship.addImage(spaceship);
+  Spaceship.scale = 0.3;
+
+  Meteor = createSprite(width/2, height-250, 50, 60);
+  Meteor.addImage(meteor);
+  Meteor.scale = 0.6;
+
+}
+
+function draw() {
+  background("black"); 
+  
+  drawSprites();
+}
